38ac3f63993c9bf44f080f5d1703d45af277ae87
Michael Bowcutt <mwbowcutt@gmail.com>
2025-12-22 22:10:32 -0500

    Generate rest of waveforms

--- main.c
+++ main.c
@@ -9,11 +9,12 @@
 /// COMPILER CONSTANTS/DEFINITIONS
 ///
 #define SAMPLE_RATE 44100
+#define WAVE_TABLE_SIZE ((size_t)(UINT16_MAX + 1))
 #define NUM_AUDIO_BUFFERS 4
 #define DEFAULT_FREQUENCY 440u
 
 #define DEBUG_AUDIO_BUFFER_STATS 0
-#define DEBUG_CONSOLE 0
+#define DEBUG_CONSOLE 1
 
 
 ///
@@ -50,7 +51,7 @@ struct Envelope_s
 
 typedef void (*HandlerFunc)(uint32_t * frequency,
                             uint32_t * tune,
-                            enum OscillatorType_e * osc_type, 
+                            enum OscillatorType_e *  osc_type, 
                             struct Envelope_s * envelope);
 
 
@@ -63,7 +64,9 @@ static size_t mix_buffer_len = 0;
 
 static float mix_gain = 0.5f;
 
-const short * sine_lut = NULL;
+static short * osc_wave_tables[NUM_OSCILLATORS];
+
+static float target_rms = 0;
 
 #if DEBUG_AUDIO_BUFFER_STATS
 static unsigned int write_cnt = 0;
@@ -76,7 +79,7 @@ static unsigned int no_write_cnt = 0;
 /// STATIC PROTOTYPES
 ///
 static void write_ai_buffer(short * buffer, size_t const num_samples,
-                            enum OscillatorType_e osc_type,
+                            short * wave_table,
                             uint32_t * phase,
                             uint32_t const tune,
                             struct Envelope_s * envelope);
@@ -89,7 +92,7 @@ static short interpolate_delta(int16_t const y0,
 
 static void audio_buffer_run(uint32_t * phase,
                              uint32_t const tune,
-                             enum OscillatorType_e osc_type,
+                             short * wave_table,
                              struct Envelope_s * envelope);
 
 static void graphics_draw(enum OscillatorType_e osc_type, uint32_t const frequency);
@@ -98,23 +101,138 @@ static void envelope_tick(struct Envelope_s * envelope);
 
 static HandlerFunc input_poll(void);
 
-static short * generate_sine_lut(size_t lut_size);
+static short * generate_sine_lut(void);
+static short * generate_square_lut(size_t num_harmonics);
+static short * generate_triangle_lut(size_t num_harmonics);
+static short * generate_ramp_lut(size_t num_harmonics);
 
 
 /// 
 /// FUNCTION DEFINITIONS
 ///
-static short * generate_sine_lut(size_t lut_size)
+static short * generate_sine_lut(void)
 {
-    short * lut = malloc(lut_size * sizeof(short));
+    short * lut = malloc(WAVE_TABLE_SIZE * sizeof(short));
     if (!lut) return NULL;
 
-    float const phase_step = (2.0f * M_PI) / (float)lut_size;
+    short max_amplitude = INT16_MAX / 2;
 
-    for (size_t i = 0; i < lut_size; ++i)
+    float const phase_step = (2.0f * M_PI) / WAVE_TABLE_SIZE;
+
+    for (size_t i = 0; i < WAVE_TABLE_SIZE; ++i)
     {
-        lut[i] = (short)(INT16_MAX * sinf((float)i * phase_step));
+        lut[i] = (short)(max_amplitude * sinf((float)i * phase_step));
     }
+
+    return lut;
+}
+
+static short * generate_square_lut(size_t num_harmonics)
+{
+    short * lut = malloc(WAVE_TABLE_SIZE * sizeof(short));
+    if (!lut) return NULL;
+    float * temp_lut = malloc(WAVE_TABLE_SIZE * sizeof(float));
+    if (!lut) return NULL;
+
+    float const phase_step = (2.0f * M_PI) / WAVE_TABLE_SIZE;
+    
+    float sum_squares = 0;
+
+    for (size_t i = 0; i < WAVE_TABLE_SIZE; ++i)
+    {
+        temp_lut[i] = 0;
+        for (size_t h = 1; h <= num_harmonics; h+=2)
+        {
+            temp_lut[i] += sinf((float)i * phase_step * h) / h;
+        }
+        sum_squares += temp_lut[i] * temp_lut[i];
+    }
+
+    float rms = sqrtf(sum_squares / WAVE_TABLE_SIZE);
+    float scale = target_rms / rms;
+
+    printf("Target: %f, RMS: %f, scale: %f\n", target_rms, rms, scale);
+
+    for (size_t i = 0; i < WAVE_TABLE_SIZE; ++i)
+    {
+        lut[i] = (short)(temp_lut[i] * scale * INT16_MAX);
+    }
+
+    free(temp_lut);
+
+    return lut;
+}
+
+static short * generate_triangle_lut(size_t num_harmonics)
+{
+    short * lut = malloc(WAVE_TABLE_SIZE * sizeof(short));
+    if (!lut) return NULL;
+    float * temp_lut = malloc(WAVE_TABLE_SIZE * sizeof(float));
+    if (!lut) return NULL;
+
+    float const phase_step = (2.0f * M_PI) / WAVE_TABLE_SIZE;
+
+    float sum_squares = 0;
+
+    for (size_t i = 0; i < WAVE_TABLE_SIZE; ++i)
+    {
+        temp_lut[i] = 0;
+        float sign = 1.0f;
+        for (size_t h = 1; h <= num_harmonics; h+=2)
+        {
+            temp_lut[i] += (sign * sinf((float)i * phase_step * h) / (float)(h * h));
+            sign *= -1.0f;
+        }
+        sum_squares += temp_lut[i] * temp_lut[i];
+    }
+
+    float rms = sqrtf(sum_squares / WAVE_TABLE_SIZE);
+    float scale = target_rms / rms;
+
+    printf("Target: %f, RMS: %f, scale: %f\n", target_rms, rms, scale);
+
+    for (size_t i = 0; i < WAVE_TABLE_SIZE; ++i)
+    {
+        lut[i] = (short)(temp_lut[i] * scale * INT16_MAX);
+    }
+
+    free(temp_lut);
+
+    return lut;
+}
+
+static short * generate_ramp_lut(size_t num_harmonics)
+{
+    short * lut = malloc(WAVE_TABLE_SIZE * sizeof(short));
+    if (!lut) return NULL;
+    float * temp_lut = malloc(WAVE_TABLE_SIZE * sizeof(float));
+    if (!lut) return NULL;
+
+    float const phase_step = (2.0f * M_PI) / WAVE_TABLE_SIZE;
+
+    float sum_squares = 0;
+
+    for (size_t i = 0; i < WAVE_TABLE_SIZE; ++i)
+    {
+        temp_lut[i] = 0;
+        for (size_t h = 1; h <= num_harmonics; ++h)
+        {
+            temp_lut[i] += sinf((float)i * phase_step * h) / (float)h;
+        }
+        sum_squares += temp_lut[i] * temp_lut[i];
+    }
+
+    float rms = sqrtf(sum_squares / WAVE_TABLE_SIZE);
+    float scale = target_rms / rms;
+
+    printf("Target: %f, RMS: %f, scale: %f\n", target_rms, rms, scale);
+
+    for (size_t i = 0; i < WAVE_TABLE_SIZE; ++i)
+    {
+        lut[i] = (short)(temp_lut[i] * scale * INT16_MAX);
+    }
+
+    free(temp_lut);
 
     return lut;
 }
@@ -139,13 +257,13 @@ static short interpolate_delta(int16_t const y0,
     return (short) (((int64_t)((int32_t)(y1 - y0) * (int32_t)frac)) >> 16);
 }
 
-static short sine_component(uint32_t const phase)
+static short phase_to_amplitude(uint32_t const phase, short * wave_table)
 {
     uint16_t const phase_int = (uint16_t const)((phase) >> 16);
     uint16_t const phase_frac = (uint16_t const)phase;
 
-    short const y0 = sine_lut[phase_int];
-    short const y1 = sine_lut[phase_int + 1]; // Wrap around
+    short const y0 = wave_table[phase_int];
+    short const y1 = wave_table[phase_int + 1]; // Wrap around
 
     return y0 + interpolate_delta(y0, y1, phase_frac);
 }
@@ -180,7 +298,7 @@ static short ramp_component(uint32_t const phase)
 
 
 static void write_ai_buffer(short * buffer, size_t const num_samples,
-                            enum OscillatorType_e osc_type,
+                            short * wave_table,
                             uint32_t * phase,
                             uint32_t const tune,
                             struct Envelope_s * envelope)
@@ -192,33 +310,7 @@ static void write_ai_buffer(short * buffer, size_t const num_samples,
             short amplitude = 0;
 
             // For each voice:
-            short component = 0u;
-            switch (osc_type)
-            {
-                case SINE:
-                {
-                    component = sine_component(*phase);
-                    break;
-                }
-                case TRIANGLE:
-                {
-                    component = triangle_component(*phase);
-                    break;
-                }
-                case SQUARE:
-                {
-                    component = square_component(*phase);
-                    break;
-                }
-                case RAMP:
-                {
-                    component = ramp_component(*phase);
-                    break;
-                }
-                case NUM_OSCILLATORS:
-                default:
-                    break;
-            }
+            short component = phase_to_amplitude(*phase, wave_table);
 
             component = (short)(((int64_t)component * (int64_t)envelope->level) / UINT32_MAX);
 
@@ -408,13 +500,13 @@ static void wave_next(uint32_t * frequency,
                       enum OscillatorType_e * osc_type,
                       struct Envelope_s * envelope)
 {
-    if ((NUM_OSCILLATORS-1) == *osc_type)
+    if (*osc_type == RAMP)
     {
         *osc_type = SINE;
     }
     else
     {
-        (*osc_type)++;
+        ++(*osc_type);
     }
 }
 
@@ -423,13 +515,13 @@ static void wave_prev(uint32_t * frequency,
                       enum OscillatorType_e * osc_type,
                       struct Envelope_s * envelope)
 {
-    if (SINE == *osc_type)
+    if (*osc_type == SINE)
     {
-        *osc_type = NUM_OSCILLATORS - 1;
+        *osc_type = RAMP;
     }
     else
     {
-        (*osc_type)--;
+        --(*osc_type);
     }
 }
 
@@ -503,7 +595,7 @@ static HandlerFunc input_poll(void)
 
 static void audio_buffer_run(uint32_t * phase,
                              uint32_t const tune,
-                             enum OscillatorType_e osc_type,
+                             short * wave_table,
                              struct Envelope_s * envelope)
 {
     if (audio_can_write()) {
@@ -519,7 +611,7 @@ static void audio_buffer_run(uint32_t * phase,
         else
         {
             write_ai_buffer(buffer, audio_get_buffer_length(),
-                            osc_type,
+                            wave_table,
                             phase, tune,
                             envelope);
         }
@@ -531,7 +623,7 @@ static void audio_buffer_run(uint32_t * phase,
         ++local_write_cnt;
 #endif
         write_ai_buffer(mix_buffer, audio_get_buffer_length(),
-                        osc_type,
+                        wave_table,
                         phase, tune,
                         envelope);
         mix_buffer_full = true;
@@ -560,6 +652,7 @@ int main(void) {
         .release_samples = SAMPLE_RATE,  // 1 second release
     };
 
+
 	display_init(RESOLUTION_512x240, DEPTH_16_BPP, 3, GAMMA_NONE, FILTERS_RESAMPLE);
     draw_splash_loading();
 
@@ -567,13 +660,29 @@ int main(void) {
 	debug_init_isviewer();
 	debug_init_usblog();
 
-    sine_lut = generate_sine_lut(65536);
-    if (!sine_lut) return -1;
-
 #if DEBUG_CONSOLE
     console_init();
     console_set_render_mode(RENDER_AUTOMATIC);
 #endif
+
+    target_rms = sqrtf(2.0f) / 4;
+
+    osc_wave_tables[SINE] = generate_sine_lut();
+    if (!osc_wave_tables[SINE]) return -1;
+    else printf("Sine generated\n");
+
+    size_t const num_harmonics = 60;
+    osc_wave_tables[SQUARE] = generate_square_lut(num_harmonics);
+    if (!osc_wave_tables[SQUARE]) return -1;
+    else printf("Square generated\n");
+
+    osc_wave_tables[TRIANGLE] = generate_triangle_lut(num_harmonics);
+    if (!osc_wave_tables[TRIANGLE]) return -1;
+    else printf("Triangle generated\n");
+
+    osc_wave_tables[RAMP] = generate_ramp_lut(num_harmonics);
+    if (!osc_wave_tables[RAMP]) return -1;
+    else printf("Ramp generated\n");
 
 	audio_init(SAMPLE_RATE, NUM_AUDIO_BUFFERS);
     tune = get_tune(frequency);
@@ -596,7 +705,7 @@ int main(void) {
             graphics_draw(osc_type, frequency);
         }
 
-        audio_buffer_run(&phase, tune, osc_type, &envelope);
+        audio_buffer_run(&phase, tune, osc_wave_tables[osc_type], &envelope);
     }
 
     if (mix_buffer) free(mix_buffer);
